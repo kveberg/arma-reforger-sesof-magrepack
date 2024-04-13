b115e8cc4b871ae7fb245b528bae31dd19cc8ed3
Torbjørn Kveberg <tkvebbs@gmail.com>
2024-04-13 23:03:12 +0200

    Update SCR_PlayerController.c

--- SCR_PlayerController.c
+++ SCR_PlayerController.c
@@ -1,10 +1,16 @@
 modded class SCR_PlayerController
-{
-	void CombineMags(MagazineComponent fromMag, MagazineComponent toMag, SCR_InventoryStorageManagerComponent managerComp)
+{	
+	void RepackMags(MagazineComponent fromMag, MagazineComponent toMag, SCR_InventoryStorageManagerComponent managerComp, BaseInventoryStorageComponent storageTo, BaseInventoryStorageComponent storageFrom)
 	{
 		if(!fromMag || !toMag)
 		{
 			Print("Could not locate magazine components", LogLevel.ERROR);
+			return;
+		}
+		
+		if(!managerComp)
+		{
+			Print("Was unable to locate storage or manager components", LogLevel.ERROR);
 			return;
 		}
 		
@@ -14,59 +20,68 @@ modded class SCR_PlayerController
 		
 		IEntity fromEntity = fromMag.GetOwner();
 		IEntity toEntity = toMag.GetOwner();
-		IEntity owner = managerComp.GetOwner();
-		BaseInventoryStorageComponent storage = BaseInventoryStorageComponent.Cast(owner.FindComponent(BaseInventoryStorageComponent));
+
+		Print("fromMag holds: " + fromCount + " rounds.");
+		Print("toMag holds: " + toCount + " rounds.");
 		
-		if(!storage)
-		{
-			Print("Was unable to locate storage or manager components", LogLevel.ERROR);
-			return;
-		}
-		
+			
 		if(fromCount + toCount <= maxCount)
 		{
 			toMag.SetAmmoCount(fromCount + toCount);
-
-			// Removing + Adding appears to make the magazine "stack" or "refresh"
-			if(managerComp.TryRemoveItemFromInventory(toEntity, storage))
+			Print("Added " + fromCount + " rounds to toMag. It now holds " + (fromCount + toCount) + " rounds. fromMag was fully exhausted.");
+			
+			
+			// Reinsert mag in optimal slot to help stacking and updating
+			if(managerComp.TryRemoveItemFromInventory(toEntity, storageTo))
 			{
+				Print("Underflow situation. Mag removed from inventory. Inserting.");
 				// Just print something if for whatever reason we can't add the magazine back
-				if(!managerComp.TryInsertItemInStorage(toEntity, storage))
+				if(!managerComp.TryInsertItemInStorage(toEntity, managerComp.FindStorageForItem(toEntity, EStoragePurpose.PURPOSE_ANY)))
+				//if(!managerComp.TryInsertItemInStorage(toEntity, storageTo))
 				{
-					Print("Was unable to successfully add packed magazine back into storage", LogLevel.ERROR);
+					Print("Was unable to successfully add packed magazine back into storage. Trying to find new storage for item.", LogLevel.WARNING);
 				}
 			}
+
+			// fromMag is exhausted and deleted	
+			//SCR_EntityHelper.DeleteEntityAndChildren(fromEntity);
 			
-			// From mag was fully exhausted 			
-			SCR_EntityHelper.DeleteEntityAndChildren(fromMag.GetOwner());
+			// fromMag is exhausted and dropped on the ground
+			managerComp.TryRemoveItemFromInventory(fromEntity);
+				
 		}
 		else
-		{
+		{		
+			
 			int remainder = (fromCount + toCount) % maxCount;
+			Print("Added " + (maxCount - toCount) + " rounds to toMag. It is now full. The remainder in fromMag is " + remainder);
 			
 			toMag.SetAmmoCount(maxCount);
-
-			// Removing + Adding appears to make the magazines "stack" or "refresh"
-			if(managerComp.TryRemoveItemFromInventory(toEntity, storage))
+			
+			// Reinsert mag in optimal slot to help stacking and updating
+			if(managerComp.TryRemoveItemFromInventory(toEntity, storageTo))
 			{
-				// Just print something if for whatever reason we can't add the magazine back
-				if(!managerComp.TryInsertItemInStorage(toEntity, storage))
+				if(!managerComp.TryInsertItemInStorage(toEntity, managerComp.FindStorageForItem(toEntity, EStoragePurpose.PURPOSE_ANY)))
 				{
 					Print("Was unable to successfully add packed magazine back into storage", LogLevel.ERROR);
 				}
 			}
+
+			
 			
 			if(remainder > 0)
 			{
 				fromMag.SetAmmoCount(remainder);
 				
-				if(managerComp.TryRemoveItemFromInventory(fromEntity, storage))
+
+				if(managerComp.TryRemoveItemFromInventory(fromEntity, storageFrom))
 				{
-					if(!managerComp.TryInsertItemInStorage(fromEntity, storage))
+					if(!managerComp.TryInsertItemInStorage(fromEntity, storageFrom))
 					{
 						Print("Was unable to successfully add packed magazine back into storage", LogLevel.ERROR);
 					}
 				}
+		
 			}				
 			else 
 				SCR_EntityHelper.DeleteEntityAndChildren(fromMag.GetOwner());
@@ -74,33 +89,43 @@ modded class SCR_PlayerController
 	}	
 	
 	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
-	void RpcAsk_CombineMags(RplId fromMagazineComponent, RplId toMagazineComponent, RplId storageManagerComponent)
+	void RpcAsk_RepackMags(RplId fromMag, RplId toMag, RplId managerComp, RplId storageTo, RplId storageFrom)
 	{
-		/*
-  		    We just need the two magazines which need tweaking, along with the storage manager.
- 		*/
-		if(!fromMagazineComponent.IsValid())
+  		// Make sure we have everything
+		if(!fromMag.IsValid())
 		{
 			Print("Invalid from magazine", LogLevel.ERROR);
 			return;
 		}
 		
-		if(!toMagazineComponent.IsValid())
+		if(!toMag.IsValid())
 		{
 			Print("Invalid to magazine", LogLevel.ERROR);
 			return;
 		}
 		
-		if(!storageManagerComponent.IsValid())
+		if(!managerComp.IsValid())
 		{
 			Print("Invalid manager", LogLevel.ERROR);
 			return;
 		}
 		
-		MagazineComponent fromMag = MagazineComponent.Cast(Replication.FindItem(fromMagazineComponent));
-		MagazineComponent toMag = MagazineComponent.Cast(Replication.FindItem(toMagazineComponent));
-		SCR_InventoryStorageManagerComponent managerComponent = SCR_InventoryStorageManagerComponent.Cast(Replication.FindItem(storageManagerComponent));
+		if(!storageTo.IsValid())
+		{
+			Print("Invalid storageTo", LogLevel.ERROR);
+		}
 		
-		CombineMags(fromMag, toMag, managerComponent);		
-	}
+		if(!storageFrom.IsValid())
+		{
+			Print("Invalid storageFrom", LogLevel.ERROR);
+		}
+		
+		RepackMags(
+			MagazineComponent.Cast(Replication.FindItem(fromMag)),
+			MagazineComponent.Cast(Replication.FindItem(toMag)),
+			SCR_InventoryStorageManagerComponent.Cast(Replication.FindItem(managerComp)),
+			BaseInventoryStorageComponent.Cast(Replication.FindItem(storageTo)),
+			BaseInventoryStorageComponent.Cast(Replication.FindItem(storageFrom))
+			);
+	}	
 };
